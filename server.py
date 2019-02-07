80ba396e4c96c5e3fb12515425d6d13417b67c81
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-06 18:04:07 -0800

    Finished handling login and logout

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 """Movie Ratings."""
 
 from jinja2 import StrictUndefined
-
-from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
-
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
 
@@ -22,7 +20,67 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register", methods= ["GET"])
+def register_form():
+    """Show registeration form to put in email address and password"""
+
+    return render_template("register_form.html")
+
+@app.route("/register_process", methods= ["POST"])
+def register_process():
+    """Process the registration form, checking to see if user exists"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    print(User.query.filter_by(email=email).first())
+    if User.query.filter_by(email=email).first()==[]:
+        user = User(email=email,
+                    password=password)
+        db.session.add(user)
+    else:
+        flash("You've already registered. Please login.")
+
+    db.session.commit()
+
+    return redirect("/")
+
+@app.route("/login", methods = ['GET','POST'])
+def login():
+    """Get and process login info"""
+
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        sql_password = user.password
+
+        if password == sql_password:
+            session['user_id'] = user.user_id
+            flash("Logged in.")
+            return redirect("/")
+        else:
+            flash("Invalid credentials")
+
+    return render_template('login.html')
+
+@app.route("/logout", methods = ['POST'])
+def logout():
+    """Get and process logout info"""
+
+    session.pop('user_id')
+    flash("Logged out.")
+    return redirect("/")
+
 
 
 if __name__ == "__main__":
@@ -36,5 +94,5 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
-
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     app.run(port=5000, host='0.0.0.0')
